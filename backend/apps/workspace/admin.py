dcdfe09a24dc8e82f929e3a6e60340de73b02b08
Kanishk Yadav <yadavkanishk57@gmail.com>
2026-06-23 02:24:17 +0530

    Implement channel model

--- backend/apps/workspace/admin.py
+++ backend/apps/workspace/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Workspace, WorkspaceMember, Channel
+
+admin.site.register(Workspace)
+admin.site.register(WorkspaceMember)
+admin.site.register(Channel)
