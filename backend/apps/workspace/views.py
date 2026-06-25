c0c72fa77a506bfdc70e1ca82ff34fb7cd894581
Kanishk Yadav <yadavkanishk57@gmail.com>
2026-06-25 06:47:57 +0530

    Implement initial REST APIs

--- backend/apps/workspace/views.py
+++ backend/apps/workspace/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Workspace
+from .serializers import WorkspaceSerializer
+
+
+class WorkspaceViewSet(viewsets.ModelViewSet):
+    queryset = Workspace.objects.all()
+    serializer_class = WorkspaceSerializer
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.workspace',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
